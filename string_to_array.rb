9e39efe7b063baed2cb556e5d06ac16a2986fec6
Uthaeus <romanlavery@gmail.com>
2018-05-31 14:50:52 -0600

    Write a function to split a string and convert it into an array of words

---
+++ string_to_array.rb
@@ -0,0 +1,11 @@
+# Write a function to split a string and convert it into an array of words. For example:
+
+
+
+def string_to_array(string)
+  #your code here
+  return string.split(' ')
+end
+
+
+p string_to_array("I love arrays they are my favorite") #, ["I", "love", "arrays", "they", "are", "my", "favorite"]
