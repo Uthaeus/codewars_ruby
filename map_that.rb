fef151ddd1fe4b52548f43b3092ee8223f115885
Uthaeus <romanlavery@gmail.com>
2018-07-25 10:29:30 -0600

    Create a method flat_map that accepts a list and a block, runs the block for each item in the list

---
+++ map_that.rb
@@ -0,0 +1,13 @@
+# Create a method flat_map that accepts a list and a block, runs the block for each item in the list. It should return a new array with the block return values, and that array should be flattened (1-dimensional).
+
+
+
+def flat_map list, &block
+  # your solution here
+  list.flat_map &block
+end
+
+
+
+list = [3,5,8,13]
+p flat_map(list){|item| item} #, list
