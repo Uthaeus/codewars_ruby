2defd38521ab809735c7f120f613ffd1f3b67c8d
Uthaeus <romanlavery@gmail.com>
2018-08-12 22:30:05 -0600

    Interval_union and trie exercises

---
+++ 6kyu/interval_union.rb
@@ -0,0 +1,14 @@
+# Write a function interval_insert which takes as input
+
+# a list myl of disjoint closed intervals with integer endpoints, sorted by increasing order of left endpoints
+# an interval interval
+# and returns the union of interval with the intervals in myl, as an array of disjoint closed intervals.
+
+
+def interval_insert (myl, interval)
+    #your code here
+end
+
+
+
+p interval_insert([[1, 2]], [3, 4]) #, [[1, 2], [3, 4]])
---
+++ 6kyu/trie.rb
@@ -0,0 +1,12 @@
+# The goal of this interview is to implement trie (or prefix tree) using dictionaries, where:
+
+# the dictionary keys are the prefixes
+# the value of a leaf node is None in Python and nil in Ruby.
+
+
+def build_trie(*words)
+  puts "Implement me! :)"
+end
+
+
+
