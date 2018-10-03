f2a693d0160d2850e740616273340a98ad158791
Uthaeus <romanlavery@gmail.com>
2018-10-03 09:41:19 -0600

    Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it

---
+++ 7kyu/making_copies.rb
@@ -0,0 +1,12 @@
+# Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+
+def copy_list(l)
+    copy = [*l]
+    copy 
+end
+
+
+
+t = [1, 2, 3, 4]
+p copy_list(t)
