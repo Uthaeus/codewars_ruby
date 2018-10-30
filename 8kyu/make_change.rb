83f43f1efe1cc2155ed00e248a6c9d352343675e
Uthaeus <romanlavery@gmail.com>
2018-10-30 15:55:43 -0600

    Create a method partition that accepts a list and a block. It should return two arrays: the first, with all the elements for which the given block returned true, and the second for the remaining elements

---
+++ 8kyu/make_change.rb
@@ -0,0 +1,13 @@
+# You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
+
+
+get_change ={
+    money = 10,
+    candy = 1.42,
+    chips = 2.40,
+    soda = 1,
+    change =  
+}
+
+
+
