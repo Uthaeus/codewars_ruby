e6fa6018229f012cac0cf2c856b377f7f7df2148
Uthaeus <romanlavery@gmail.com>
2019-03-22 09:33:08 -0600

    You want to create secret messages which can be deciphered by the Decipher this! kata

--- 6kyu/encrypt_this.rb
+++ 6kyu/encrypt_this.rb
@@ -2,10 +2,32 @@
 
 
 def encrypt_this(text)
-    # your code here
+    arr = text.split(' ')
+    result = []
+
+    arr.each do |a|
+        word = a.split('')
+        temp = []
+        temp << word[0].ord
+        x = 1
+        while x < word.length do 
+            if x == 1
+                temp << word.last
+            elsif x == word.length - 1
+                temp << word[1]
+            else
+                temp << word[x]
+            end
+            x += 1
+        end
+        result << temp.join('')
+    end
+    result.join(' ')
 end
 
 
 
 p encrypt_this("A wise old owl lived in an oak")
 # "65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
+p encrypt_this("The more he saw the less he spoke")
+# "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
---
+++ 6kyu/unique_num.rb
@@ -0,0 +1,1 @@
+# There is an array with some numbers. All numbers are equal except for one. Try to find it!
