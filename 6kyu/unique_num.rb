d9da27612287a9b21ada4b525baab05e58bc7fff
Uthaeus <romanlavery@gmail.com>
2019-03-22 09:56:15 -0600

    There is an array with some numbers. All numbers are equal except for one. Try to find it

--- 6kyu/unique_num.rb
+++ 6kyu/unique_num.rb
@@ -1,1 +1,18 @@
 # There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+
+def find_uniq(arr)
+    res = {}
+    arr.each do |a|
+        if res[a]
+            res[a] += 1
+        else
+            res[a] = 1
+        end
+    end
+    res.find {|key, val| val == 1}.first
+end
+
+
+
+p find_uniq([ 0, 0, 0.55, 0, 0 ]) #, 0.55
