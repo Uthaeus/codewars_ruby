58692ae68ebdd0a42078c3931f9179a690a78a6e
Uthaeus <romanlavery@gmail.com>
2020-08-18 15:23:40 -0600

    Your function must return an array that contains the numbers in order to generate the given section of FizzBuzz

---
+++ 6kyu/rev_fizzbuzz.rb
@@ -0,0 +1,39 @@
+# Write a function that accepts a string, which will always be a valid section of FizzBuzz. Your function must return an array that contains the numbers in order to generate the given section of FizzBuzz.
+
+
+def reverse_fizzbuzz(s)
+    check_str = []
+    result = []
+    len = s.split(' ').length
+
+    x = 1
+    loop do
+        if x % 3 == 0 && x % 5 == 0
+            check_str << "FizzBuzz"
+        elsif x % 5 == 0
+            check_str << "Buzz"
+        elsif x % 3 == 0
+            check_str << "Fizz"
+        else
+            check_str << x
+        end
+
+        result << x 
+        temp = check_str.slice(-len)
+
+        p check_str
+        p temp
+        p check_str.slice(-3)
+
+        if temp == s 
+            return result.slice(-len, len)
+        end
+        
+
+        x += 1
+    end
+
+end
+
+
+p reverse_fizzbuzz("1 2 Fizz 4 Buzz") #, [1, 2, 3, 4, 5]
--- 6kyu/reverse_fizzbuzz.rb
+++ 6kyu/reverse_fizzbuzz.rb
@@ -1,38 +1,36 @@
-# Write a function that accepts a string, which will always be a valid section of FizzBuzz. Your function must return an array that contains the numbers in order to generate the given section of FizzBuzz.
 
 
-def reverse_fizzbuzz(s)
+def reverse_fizzbuzz(s) 
     check_str = []
     result = []
     len = s.split(' ').length
 
     x = 1
-    loop do
+    loop do 
         if x % 3 == 0 && x % 5 == 0
-            check_str << "FizzBuzz"
+            check_str << 'FizzBuzz'
         elsif x % 5 == 0
-            check_str << "Buzz"
+            check_str << 'Buzz'
         elsif x % 3 == 0
-            check_str << "Fizz"
+            check_str << 'Fizz'
         else
             check_str << x
         end
 
-        result << x 
-        temp = check_str.slice(-len)
+        result << x
 
-        p check_str
-        p temp
-        p check_str.slice(-3)
+        if check_str.length >= len 
 
-        if temp == s 
-            return result.slice(-len, len)
+            temp  = check_str.slice(check_str.length - len, check_str.length)
+
+            if temp.join(' ') == s 
+                return result.slice(result.length - len, result.length)
+            end
         end
-
         x += 1
     end
-
 end
 
 
 p reverse_fizzbuzz("1 2 Fizz 4 Buzz") #, [1, 2, 3, 4, 5]
+p reverse_fizzbuzz("Fizz 688 689 FizzBuzz") #, [687, 688, 689, 690]
