4b2a9dd11791da0afc6500b69cd90bc0517b98cc
Uthaeus <romanlavery@gmail.com>
2020-10-06 12:59:17 -0600

    testing if/else logic

--- 6kyu/generala.rb
+++ 6kyu/generala.rb
@@ -7,6 +7,7 @@ def points(dice)
     three = 0
     two = 0
     one = 0
+    result = 0
 
     num_hash = {}
     num_arr = dice.split('')
@@ -33,7 +34,26 @@ def points(dice)
             one += 1
         end
     end
-
+    
+    if five == 1
+        return 50
+    elsif four == 1
+        return 40
+    elsif three == 1 && two == 1
+        return 30
+    elsif one >= 5
+        sorted = num_arr.map { |x| x.to_i }.sort
+        m = 0
+        while m < sorted.length - 1 do 
+            if sorted[m] + 1 != sorted[m + 1]
+                return 0
+            end
+            m += 1
+        end
+        return 20
+    else
+        return 0
+    end
 end
 
 
